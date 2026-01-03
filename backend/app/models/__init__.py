d4bf396059eb23ca0847cd8fd9c0c722d889d03a
SHOU6439 <shian35777@gmail.com>
2026-01-03 18:56:40 +0900

    initファイルに新しく追加したモデルを追記

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -6,5 +6,6 @@ from .tag import Tag
 from .post import Post
 from .post_tags import post_tags
 from .post_products import post_products
+from .post_assets import post_assets
 
-__all__ = ["User", "RefreshToken", "Asset", "Product", "Tag", "Post", "post_tags", "post_products"]
+__all__ = ["User", "RefreshToken", "Asset", "Product", "Tag", "Post", "post_tags", "post_assets", "post_products"]
